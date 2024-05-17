3c9946e3e888957902527a0365e06b058bb9e9f2
alxedv <alexmaia.etel@gmail.com>
2024-05-17 01:06:20 -0300

    feat: add sign in and sign out forms

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,19 @@
+'use server';
+
+export const signIn = async () => {
+  try {
+
+  } catch (error) {
+    console.error(error);
+
+  }
+}
+
+export const signUp = async (useData: SignUpParams) => {
+  try {
+
+  } catch (error) {
+    console.error(error);
+
+  }
+}
