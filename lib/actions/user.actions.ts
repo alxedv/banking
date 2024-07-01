f0430130bdca3c32484390e630f1bfc7dd1e3ba8
alxedv <alexmaia.etel@gmail.com>
2024-07-01 16:07:32 -0300

    feat: implement auth, appwrite and sentry

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,17 @@
 'use server';
 
-export const signIn = async () => {
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
+export const signIn = async ({ email, password }: signInProps) => {
   try {
+    const { account } = await createAdminClient();
+
+    const response = await account.createEmailPasswordSession(email, password);
+
+    return parseStringify(response);
 
   } catch (error) {
     console.error(error);
@@ -9,11 +19,54 @@ export const signIn = async () => {
   }
 }
 
-export const signUp = async (useData: SignUpParams) => {
+export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
+
   try {
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount);
 
   } catch (error) {
     console.error(error);
+
+  }
+}
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    const user = await account.get();
+
+    return parseStringify(user);
+  } catch (error) {
+    return null;
+  }
+}
+
+export const loggoutAccount = async () => {
+  try {
+    const { account } = await createSessionClient();
+
+    cookies().delete('appwrite-session');
+
+    await account.deleteSession('current');
+  } catch (error) {
 
   }
 }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,5 @@
 "use server";
-import { Client, Account } from "node-appwrite";
+import { Client, Account, Databases, Users } from "node-appwrite";
 import { cookies } from "next/headers";
 
 export async function createSessionClient() {
@@ -7,7 +7,7 @@ export async function createSessionClient() {
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-  const session = cookies().get("my-custom-session");
+  const session = cookies().get("appwrite-session");
   if (!session || !session.value) {
     throw new Error("No session");
   }
@@ -21,8 +21,6 @@ export async function createSessionClient() {
   };
 }
 
-// comentario teste
-
 export async function createAdminClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
@@ -33,5 +31,12 @@ export async function createAdminClient() {
     get account() {
       return new Account(client);
     },
+    get database() {
+      return new Databases(client);
+    },
+    get user() {
+      return new Users(client);
+    }
+
   };
 }
